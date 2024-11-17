c53f6505bd816a21b509a4b6fbcda6153bd706aa
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-11-16 19:14:30 -0600

    feat: Add bin target to project
    The user can now create a binary instead of just building the library
    and running tests. This allows this tool to be used in scripts.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use stringdiff::{annotate_strings, colorize};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <left> <right>", args[0]);
+        std::process::exit(1);
+    }
+    let result = annotate_strings(&args[1], &args[2]);
+    colorize(&result);
+}
